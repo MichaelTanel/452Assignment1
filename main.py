6033042910633727bf8af191194d544254d4dd8e
michael-10 <mtanel@live.ca>
2019-01-29 01:24:42 -0500

    Created different neurons with different weights between 0 and 1

--- main.py
+++ main.py
@@ -4,22 +4,30 @@ import pandas as pd
 import random
 
 class Neuron(object):
-    area = 0
-    weigthArea = 0
-    perimeter = 0
-    weightPerimeter = 0
-    compactness = 0
-    weightCompactness = 0
-    length = 0
-    weightLength = 0
-    width = 0
-    weightWidth = 0
-    asymmetryCoefficient = 0
-    weightAsymmetryCoefficient = 0
-    lengthGroove = 0
-    weightLengthGroove = 0
-    expectedOutput = 0
-    output = ""
+    def __init__(self):
+        self.area = 0
+        self.weightArea = random.random()
+        self.perimeter = 0
+        self.weightPerimeter = random.random()
+        self.compactness = 0
+        self.weightCompactness = random.random()
+        self.length = 0
+        self.weightLength = random.random()
+        self.width = 0
+        self.weightWidth = random.random()
+        self.asymmetryCoefficient = 0
+        self.weightAsymmetryCoefficient = random.random()
+        self.lengthGroove = 0
+        self.weightLengthGroove = random.random()
+        self.expectedOutput = 0
+        self.output = ""
+
+    # def __init__(self):
+    #     self._weightArea = None
+
+    # @weightArea.setter
+    # def weightArea(self, value):
+    #     self._weightArea = value
 
 class MaxValues(object):
     area = 0
@@ -45,7 +53,7 @@ def normalizeData(maxVals, df):
 def importCSV(filename):
     df = pd.read_csv(filename)
     maxVals = MaxValues()
-    maxVals.area = max(df['Area']) #you can also use df['column_name']
+    maxVals.area = max(df['Area'])
     maxVals.perimeter = max(df['Perimeter'])
     maxVals.compactness = max(df['Compactness'])
     maxVals.length = max(df['Length'])
@@ -55,36 +63,51 @@ def importCSV(filename):
 
     return normalizeData(maxVals, df)
 
-def initializeNeuron():
-    neuron = Neuron()
-    neuron.weightArea = random.uniform(0, 1)
-    neuron.weightPerimeter = random.uniform(0, 1)
-    neuron.weightCompactness = random.uniform(0, 1)
-    neuron.weightLength = random.uniform(0, 1)
-    neuron.weightWidth = random.uniform(0, 1)
-    neuron.weightAsymmetryCoefficient = random.uniform(0, 1)
-    neuron.weightLengthGroove = random.uniform(0, 1)
+def initializeNeuron(neuron):
+    neuron.weightArea = random.random()
+    neuron.weightPerimeter = random.random()
+    neuron.weightCompactness = random.random()
+    neuron.weightLength = random.random()
+    neuron.weightWidth = random.random()
+    neuron.weightAsymmetryCoefficient = random.random()
+    neuron.weightLengthGroove = random.random()
+
     return neuron
 
 def main():
     df = importCSV('trainSeeds.csv')
 
-    neuron1 = initializeNeuron()
-    neuron2 = initializeNeuron()
+    neuron1 = Neuron()
+    neuron2 = Neuron()
+
+    print(neuron1.weightArea)
+    print(neuron2.weightArea)
+
     # Iterate over dataframe rows
     for row in df.iterrows():
         # In order to get the data point, must access at [1][n], 0 < n < 8
         # This is because row[0] gives the row number, row[1] gives the
         # column name and the data point
-        neuron.area = row[1][0]
-        neuron.perimeter = row[1][1]
-        neuron.compactness = row[1][2]
-        neuron.length = row[1][3]
-        neuron.width = row[1][4]
-        neuron.asymmetryCoefficient = row[1][5]
-        neuron.lengthGroove = row[1][6]
-        neuron.expectedOutput = row[1][7]
+        neuron1.area = row[1][0]
+        neuron1.perimeter = row[1][1]
+        neuron1.compactness = row[1][2]
+        neuron1.length = row[1][3]
+        neuron1.width = row[1][4]
+        neuron1.asymmetryCoefficient = row[1][5]
+        neuron1.lengthGroove = row[1][6]
+        neuron1.expectedOutput = row[1][7]
+        
+        neuron2.area = row[1][0]
+        neuron2.perimeter = row[1][1]
+        neuron2.compactness = row[1][2]
+        neuron2.length = row[1][3]
+        neuron2.width = row[1][4]
+        neuron2.asymmetryCoefficient = row[1][5]
+        neuron2.lengthGroove = row[1][6]
+        neuron2.expectedOutput = row[1][7]
 
-        print('\n------------------------------------------------\n')
+        # print(neuron1.area)
+        # print(neuron2.area)
+        # print('\n------------------------------------------------\n')
         
 main()
