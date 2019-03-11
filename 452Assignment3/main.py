dee282a25388a3456e635fd16c9e3a110e71ab9f
michael-10 <mtanel@live.ca>
2019-03-11 18:35:03 -0400

    Split into kohonen and kmeans functions

--- 452Assignment3/main.py
+++ 452Assignment3/main.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
+from shutil import copyfile
 
 numInputNodes = 3
 numOutputNodes = 2  # 2 types of clusters
@@ -15,6 +16,7 @@ numOutputNodes = 2  # 2 types of clusters
 successCount = 0
 totalCount = 0
 learningRate = 0.5
+iterations = 10
 
 # Retrieves data from row
 def parseRow(row):
@@ -85,6 +87,7 @@ def kohonen(df):
 
     global numInputNodes
     global numOutputNodes
+    global iterations
 
     # Creates two 1D arrays with floating point numbers between -1 and 1.
     # weights1 represents the weights for the connections from all input nodes to output node 1.
@@ -112,7 +115,7 @@ def kohonen(df):
     results = [0] * len(df.index)
     prevCalcError = 0
 
-    for i in range(100):
+    for i in range(iterations):
         j = 0
         # Iterate over dataframe row
         for row in df.iterrows():
@@ -136,32 +139,24 @@ def kohonen(df):
 
         # If it is the first iteration, there's no previous error calc to compare it to.
         # If it is not the first iteration, calculate the error and compare it to the previous error.
-        # If the new error is less than previous error, stop looping.
+        # If the new error is greater than previous error, stop looping.
         if i == 0:
             prevCalcError = calcError(inputValues, weights1, weights2)
         else:
-            tmpCalcError = calcError(inputValues, weights1, weights2)
-            print(i)
-            print(prevCalcError)
-            print(tmpCalcError)
-            print("-------------------")
-            if tmpCalcError > prevCalcError:
+            newCalcError = calcError(inputValues, weights1, weights2)
+
+            if newCalcError > prevCalcError:
                 print(i)
                 break
-            prevCalcError = tmpCalcError
+            prevCalcError = newCalcError
+
+    copyfile("dataset_noclass.csv", "kohonen_results.csv")
 
     return (weights1, weights2, results)
 
-def calcDistance(inputValues, df):
-    # Retrive 2 random points as "clusters"
-    cluster1Index = randint(0, len(df.index) - 1)
-    cluster2Index = randint(0, len(df.index) - 1)
-
-    cluster1 = df.iloc[cluster1Index]
-    cluster2 = df.iloc[cluster2Index]
-
-    distance1 = math.sqrt((cluster1[0] - inputValues[0]) ** 2 + (cluster1[1] - inputValues[1]) ** 2 + (cluster1[2] - inputValues[2]) ** 2)
-    distance2 = math.sqrt((cluster2[0] - inputValues[0]) ** 2 + (cluster2[1] - inputValues[1]) ** 2 + (cluster2[2] - inputValues[2]) ** 2)
+def calcDistance(inputValues, prototype1, prototype2):
+    distance1 = math.sqrt((prototype1[0] - inputValues[0]) ** 2 + (prototype1[1] - inputValues[1]) ** 2 + (prototype1[2] - inputValues[2]) ** 2)
+    distance2 = math.sqrt((prototype2[0] - inputValues[0]) ** 2 + (prototype2[1] - inputValues[1]) ** 2 + (prototype2[2] - inputValues[2]) ** 2)
 
     if distance1 < distance2:
         return 1
@@ -170,34 +165,80 @@ def calcDistance(inputValues, df):
     else:
         return randint(1, 2)
 
+# Updates the values of the prototypes by summing the list of input vectors, and dividing them by the cluster size.
+def updatePrototype(cluster1, cluster2):
+    
+    size1 = len(cluster1)
+    size2 = len(cluster2)
+
+    # Calculate new prototype values for both clusters
+    prototype1 = [sum(x) for x in zip(*cluster1)]
+    prototype1 = np.true_divide(prototype1, size1)
+
+    prototype2 = [sum(x) for x in zip(*cluster2)]
+    prototype2 = np.true_divide(prototype2, size2)
+
+    return prototype1, prototype2
+
+def calcKMeansError(inputValues, prototype1, prototype2):
+    error = np.sum(np.square(np.subtract(inputValues, prototype1)))
+    error += np.sum(np.square(np.subtract(inputValues, prototype2)))
+    return error
+
+# Implementing the k-means algorithm
 def kMeans(df):
+
+    global iterations
     # Stores the results of the clustering function
     results = [0] * len(df.index)
     prevCalcError = 0
 
-    
+    # Retrive 2 random points as the basiss for a "cluster"
+    prototype1Index = randint(0, len(df.index) - 1)
+    prototype2Index = randint(0, len(df.index) - 1)
 
-    # for i in range(1):
-        # j = 0
+    prototype1 = [0] * 3
+    prototype2 = [0] * 3
+
+    # Gets each x, y, z value into array
+    rowVal = df.iloc[prototype1Index]
+    prototype1[0], prototype1[1], prototype1[2] = rowVal[0], rowVal[1], rowVal[2]
+
+    rowVal = df.iloc[prototype2Index]
+    prototype2[0], prototype2[1], prototype2[2] = rowVal[0], rowVal[1], rowVal[2]
+
+    cluster1 = []
+    cluster2 = []
+
+    for i in range(iterations):
+        j = 0
         # Iterate over dataframe row
-    for row in df.iterrows():
-        inputValues = parseRow(row)
-        
-        results = calcDistance(inputValues, df)
+        for row in df.iterrows():
+            inputValues = parseRow(row)
             
+            result = calcDistance(inputValues, prototype1, prototype2)
+            
+            if result == 1:
+                cluster1.append(inputValues)
+            # result == 2
+            else:
+                cluster2.append(inputValues)
+
+            j += 1
+
+        prototype1, prototype2 = updatePrototype(cluster1, cluster2)
 
         # If it is the first iteration, there's no previous error calc to compare it to.
         # If it is not the first iteration, calculate the error and compare it to the previous error.
-        # If the new error is less than previous error, stop looping.
-        # if i == 0:
-        #     prevCalcError = calcError(inputValues, weights1, weights2)
-        # else:
-        #     tmpCalcError = calcError(inputValues, weights1, weights2)
-
-        #     if tmpCalcError > prevCalcError:
-        #         print(i)
-        #         break
-        #     prevCalcError = tmpCalcError
+        # If the new error is greater than previous error, stop looping.
+        if i == 0:
+            prevCalcError = calcKMeansError(inputValues, prototype1, prototype2)
+        else:
+            newCalcError = calcKMeansError(inputValues, prototype1, prototype2)
+            # If the 
+            if newCalcError > prevCalcError:
+                break
+            prevCalcError = newCalcError
 
     return results
 
@@ -205,7 +246,7 @@ def kMeans(df):
 def main():
     df = importCSV('dataset_noclass.csv')
     
-    # weights1, weights2, results = kohonen(df)
+    weights1, weights2, results = kohonen(df)
     kMeans(df)
 
     # percision = precision_score(expectedOutputList, actualOutputList, average='weighted')
@@ -213,8 +254,8 @@ def main():
     
     with open('output.txt', 'a') as outputFile:
         outputFile.write("\nFinal weights: ")
-        # outputFile.write(str(weights1))
-        # outputFile.write(str(weights2))
+        outputFile.write(str(weights1))
+        outputFile.write(str(weights2))
         # outputFile.write("\nPercision score: %.2f\n" % percision)
         # outputFile.write("\nRecall score: %.2f\n" % recall)
         # outputFile.write("\nConfusion matrix: \n%s" % confusion_matrix(expectedOutputList, actualOutputList))
