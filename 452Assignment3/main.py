9875b81706753af94d59189c5c04db1e63c5f420
michael-10 <mtanel@live.ca>
2019-03-11 15:59:25 -0400

    Sorted data into clusters. Iterated over x epochs

--- 452Assignment3/main.py
+++ 452Assignment3/main.py
@@ -42,20 +42,44 @@ def findWinner(net):
     maxVal[0] = max(0, net[0] - (1 / 2) * net[1])
     maxVal[1] = max(0, net[1] - (1 / 2) * net[0])
 
-    while (maxVal[0] == 0 and maxVal[1] == 0) or (maxVal[0] != 0 and maxVal[1] != 0):
-        maxVal[0] = max(0, net[0] - (1 / 2) * net[1])
-        maxVal[1] = max(0, net[1] - (1 / 2) * net[0])
+    if np.abs(maxVal[0]) < 1e-10:
+        maxVal[0] = 0
+    
+    if np.abs(maxVal[1]) < 1e-10:
+        maxVal[1] = 0
+
+    # Update values
+    while np.count_nonzero(maxVal) > 1:
+        maxVal[0] = max(0, maxVal[0] - (1 / 2) * maxVal[1])
+        maxVal[1] = max(0, maxVal[1] - (1 / 2) * maxVal[0])
+        
+        if np.abs(maxVal[0]) < 1e-10:
+            maxVal[0] = 0
+    
+        if np.abs(maxVal[1]) < 1e-10:
+            maxVal[1] = 0
+
 
     if maxVal[0] == 0:
         return 0
-    else:
+    elif maxVal[1] == 0:
         return 1
+    else:
+        return 2    # No winner
 
 def updateWeight(inputValues, weights):
     global learningRate
     weights = weights + learningRate * np.subtract(inputValues, weights)
 
     return weights
+
+def calcError(inputValues, weights1, weights2):
+    error = 0
+    for i in range(len(inputValues)):
+        error += (inputValues[i] - weights1[i]) ** 2
+        error += (inputValues[i] - weights2[i]) ** 2
+
+    return error
 
 def cluster(df):
 
@@ -86,41 +110,44 @@ def cluster(df):
 
     results = [0] * len(df.index)
     i = 0
-    print("hello")
-    # Iterate over dataframe row
-    for row in df.iterrows():
-        inputValues = parseRow(row)
-        print("------------------------")
-        print(inputValues)
-        print(weights1)
-        print(weights2)
-        net = calcNet(inputValues, weights1, weights2)
 
-        print("findwinner")
-        winner = findWinner(net)
-        print("foundwinner")
-        if winner == 0:
-            weights1 = updateWeight(inputValues, weights1)
-        else:
-            weights2 = updateWeight(inputValues, weights2)
+    for i in range(1000):
+        # Iterate over dataframe row
+        for row in df.iterrows():
+            inputValues = parseRow(row)
+            
+            net = calcNet(inputValues, weights1, weights2)
 
-        # Store the result in an array. The +1 is because winner can be 1 or 0
-        # but the results should be output 1 or 2
-        results[i] = winner + 1
-        i += 1
-    return (weights1, weights2)
+            winner = findWinner(net)
+            if winner == 0:
+                weights1 = updateWeight(inputValues, weights1)
+                # Store the result in an array. The +1 is because winner can be 1 or 0
+                results[i] = winner + 1
+            elif winner == 1:
+                weights2 = updateWeight(inputValues, weights2)
+                # Store the result in an array. The +1 is because winner can be 1 or 0
+                results[i] = winner + 1
+            else:
+                # Randomly choose a winner
+                results[i] = random.randint(1, 2)
+            
+            i += 1
+        calcError(inputValues, weights1, weights2)
+
+    return (weights1, weights2, results)
 
 def main():
     df = importCSV('dataset_noclass.csv')
     
-    weights1, weights2 = cluster(df)
-    
+    weights1, weights2, results = cluster(df)
+
     # percision = precision_score(expectedOutputList, actualOutputList, average='weighted')
     # recall = recall_score(expectedOutputList, actualOutputList, average='weighted')
     
     with open('output.txt', 'a') as outputFile:
         outputFile.write("\nFinal weights: ")
-        outputFile.write(str(weights))
+        outputFile.write(str(weights1))
+        outputFile.write(str(weights2))
         # outputFile.write("\nPercision score: %.2f\n" % percision)
         # outputFile.write("\nRecall score: %.2f\n" % recall)
         # outputFile.write("\nConfusion matrix: \n%s" % confusion_matrix(expectedOutputList, actualOutputList))
